d9dc6043b664d2df2a99039f5836397bd4111a10
myc1986 <moratinos.c@gmail.com>
2014-05-22 18:50:37 +0200

    Ajut de nouvelles classes vide - Piste - Réservation

---
+++ src/main/java/lesClasses/Piste.java
@@ -0,0 +1,13 @@
+package lesClasses;
+
+import java.util.ArrayList;
+
+import lesInterfaces.IJoueur;
+
+public class Piste {
+
+	public Piste()
+	{
+		
+	}
+}
---
+++ src/main/java/lesClasses/Reservation.java
@@ -0,0 +1,5 @@
+package lesClasses;
+
+public class Reservation {
+
+}
